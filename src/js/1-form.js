a1d1b2d3bae4779d3743c36ffd9c2cd4e3871370
MsHelo <2017mysha2017@gmail.com>
2024-05-26 14:05:04 +0200

    finish

---
+++ src/js/1-form.js
@@ -0,0 +1,38 @@
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.querySelector('input[name="email"]');
+const inputMassange = form.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+});
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Form Data:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
